c5f90fcda34afa38a2676609abafe7c0ba125eb6
shantam8 <dscharmen@aol.com>
2021-06-27 14:22:09 +0200

    mobile done

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
+let mobileMenuBtn = document.querySelector("#mobileMenuBtn");
+let mobileMenu = document.querySelector("#mobileMenu");
+let body = document.querySelector("body");
+const navButtons = ["#btnMenuProduct", "#btnMenuCompany", "#btnMenuConnect"];
 
+function toggleSubMenu(selectedBtn) {
+  navButtons.forEach((navButton) => {
+    if (navButton != selectedBtn) {
+        document
+        .querySelector(navButton)
+        .nextElementSibling.classList.remove("openSubMenu");
+    } else {
+        document
+        .querySelector(selectedBtn)
+        .nextElementSibling.classList.toggle("openSubMenu");
+    }
+  });
+}
 
-function init(){
-    console.log("Hi");
+function toggleMobileMenu() {
+  if (mobileMenuBtn.classList.contains("isOpen")) {
+    console.log("closing");
+    mobileMenuBtn.classList.remove("isOpen");
+    mobileMenu.classList.remove("openedMenu");
+    toggleSubMenu("closingSubs");
+    body.classList.remove("overflowHidden");
+  } else {
+    console.log("opening");
+    mobileMenuBtn.classList.add("isOpen");
+    mobileMenu.classList.add("openedMenu");
+    body.classList.add("overflowHidden");
+  }
+}
+
+function init() {
+  console.log("Hi");
+  mobileMenuBtn.addEventListener("click", toggleMobileMenu);
+  document
+    .querySelector("#btnMenuProduct")
+    .addEventListener("click", function () {
+      toggleSubMenu("#btnMenuProduct");
+    });
+  document
+    .querySelector("#btnMenuCompany")
+    .addEventListener("click", function () {
+      toggleSubMenu("#btnMenuCompany");
+    });
+  document
+    .querySelector("#btnMenuConnect")
+    .addEventListener("click", function () {
+      toggleSubMenu("#btnMenuConnect");
+    });
 }
 
 window.onload = init();
